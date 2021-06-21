336b41f2809d6fa578fcceb413f9536610170d83
Dendrobyte <markbacon78@gmail.com>
2021-06-21 11:55:47 -0400

    Finish up linting, create directories

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from "./logo.png";
+import logo from "./assets/logo.png";
+import React from 'react';
 import "./App.css";
 
 function App() {
   return (
-
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
