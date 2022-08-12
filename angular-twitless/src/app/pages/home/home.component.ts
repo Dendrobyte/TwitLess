eda34b24ebe5ca06c8b6f762e404cf4aa0d3f4d1
Dendrobyte <markbacon78@gmail.com>
2022-08-12 14:52:27 -0700

    backend in go

--- angular-twitless/src/app/pages/home/home.component.ts
+++ angular-twitless/src/app/pages/home/home.component.ts
@@ -15,7 +15,8 @@ export class HomeComponent implements OnInit {
 
   possibleQuotes: string[] = [
     '"Remember what it is you want your life to be and make every decision in service of that dream"',
-    '"The good things will always be here waiting for you, should you choose to return to them- but what is important now is that satiate your curiosity"'
+    '"The good things will always be here waiting for you, should you choose to return to them- but what is important now is that satiate your curiosity"',
+    '"Don\'t live in your stories, life only moves forward"'
   ];
 
   getQuoteText(): string {
---
+++ backend-twitless/main.go
@@ -0,0 +1,4 @@
+package main
+
+// Yea that's right, we're doing this in Go now.
+// Laravel felt like it had a bit too much going on and Go is much more practical for me to learn.
